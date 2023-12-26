fe86d844e0032b2ac3635b4b6d83f04c32a69768
tejanallagorla <nallagorlat@gmail.com>
2023-12-25 19:13:51 -0500

    Fixed some bugs with user sign up

--- website/__init__.py
+++ website/__init__.py
@@ -36,4 +36,3 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         with app.app_context():
             db.create_all()
-        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -54,7 +54,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
